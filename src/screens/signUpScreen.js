982a2d175a86a41f42ef8945db1ec46d60d4fd45
Steven Galvez <stikgalvez@hotmail.com>
2018-09-25 19:18:43 -0400

    html

--- src/screens/signUpScreen.js
+++ src/screens/signUpScreen.js
@@ -21,8 +21,19 @@ function signUp() {
         <svg class="go-back1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M401.4 224h-214l83-79.4c11.9-12.5 11.9-32.7 0-45.2s-31.2-12.5-43.2 0L89 233.4c-6 5.8-9 13.7-9 22.4v.4c0 8.7 3 16.6 9 22.4l138.1 134c12 12.5 31.3 12.5 43.2 0 11.9-12.5 11.9-32.7 0-45.2l-83-79.4h214c16.9 0 30.6-14.3 30.6-32 .1-18-13.6-32-30.5-32z"/></svg>
         </span>
 
-        <div class='signUpEmailDiv'><span class='signUptxtDiv'> Email </span><input type='text' class='signUpEmailInput'> </div>
-        <div class='signUpPwdDiv'><span class='signUptxtDiv'> Password </span><input type='text' class='signUpPwdInput'> </div>
+        <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username"
+               required />
+    </div>
+
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"
+               minlength="8" required
+               placeholder="8 characters minimum" />
+    </div>
+
         <div class='signUpPwdDiv'><span class='signUptxtDiv'> Repeat Password </span><input type='text' class='signUpRepeatPwdInput'> </div>
         <button class="signUpPagebtn"> Sign up </button>
     
